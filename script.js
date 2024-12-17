c92a1f439bbd41a30909c370c1789b8d4572007e
kd90301 <kyle.p.darling@gmail.com>
2024-12-16 21:19:42 -0800

    Add 16x16 flexbox grid

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("etch a sketch!")
+console.log('etch a sketch!')
+
+const container = document.querySelector ('.container');
+
+for (let i = 1; i < 257; i++) {
+  const square = document.createElement('div');
+  container.appendChild(square);}
+
+
+// container.style.display = ('flex');
+// container.style.justifyContent = ('space-between');
+
+
+// square.style.border = ('2px, solid, black')
+
+
