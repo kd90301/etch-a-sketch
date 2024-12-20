6875a57c13208ab47b3af43868ba7857617317ea
kd90301 <kyle.p.darling@gmail.com>
2024-12-20 14:34:03 -0800

    Add function to get user input on button click

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log('etch a sketch!')
 const body = document.querySelector ('body');
 const container = document.querySelector ('.container');
 
+const sizeButton = document.querySelector ('button');
+
 body.style.backgroundColor = 'gray';
 container.style.backgroundColor = 'white';
 
@@ -14,8 +16,11 @@ for (let i = 1; i < 257; i++) {
   
   square.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'black';
-  })}
+  })
+}
 
 
-
-
+sizeButton.onclick = function resizeGrid() {
+  let userInput = prompt('How many squares per side would you like? Please select a number between 4 and 100', '16')
+  console.log(userInput);
+}
